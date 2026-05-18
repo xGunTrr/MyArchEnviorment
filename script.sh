8ca3a5649294fe2254be94a63a1b37a6a7602d0e
xguntrr <josephotto2005@gmail.com>
2026-05-18 16:59:37 -0500

    feat: Adding themes for kitty and apps(google chrome, swww, sddm)

--- script.sh
+++ script.sh
@@ -5,12 +5,16 @@ set -e
 
 # Packages installation
 echo "=> Installing needed packages"
-sudo pacman -Syu --needed --noconfirm hyprland kitty wofi nano xdg-user-dirs base-devel
+sudo pacman -Syu --needed --noconfirm hyprland kitty wofi nano xdg-user-dirs base-devel sddm
 
 mkdir -p $HOME/.config/hypr
+mkdir -p $HOME/.config/kitty
 
 # Directories managment
 xdg-user-dirs-update
+
+# Enable sddm
+sudo systemctl enable sddm
 
 # yay installation
 echo "=> Installing yay"
@@ -20,8 +24,14 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay
 
-# Visual Studio Code
-yay -S --noconfirm --needed visual-studio-code-bin
+# Visual Studio Code, Google Chrome
+yay -S --noconfirm --needed visual-studio-code-bin google-chrome swww
 
 # Copying config files
 cp -f Configs/hyprland.conf $HOME/.config/hypr/hyprland.conf
+cp -f Configs/kitty.conf $HOME/.config/kitty/
+
+mkdir -p $HOME/Pictures/Wallpapers
+cp Wallpapers/totoro-wallpaper.jpg $HOME/Pictures/Wallpapers
+
+awww img $HOME/Pictures/Wallpapers
